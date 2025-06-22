6096a1f89ab18ca7b8d22f4e07dd54b47d537693
Giannisgou <gousios.giannes@gmail.com>
2025-06-22 16:44:39 +0300

    testing + authorize in some routes

--- backend/review-service/src/routes/reviewRoutes.js
+++ backend/review-service/src/routes/reviewRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers/reviewController');
+const authorize = require('../middleware/authorize');
 
 // Routes
-router.post('/requests', ctrl.createReviewRequest);
-router.get('/requests', ctrl.getAllReviewRequests);
-router.get('/instructor', ctrl.getReviewRequestsByInstructor);
-router.post('/responses', ctrl.createReviewResponse);
-router.get('/status', ctrl.getReviewStatusForStudent);
-router.get('/requests/student/:studentId', ctrl.getReviewRequestsForStudent);
+router.post('/requests', authorize(['STUDENT', 'ADMIN', 'INST_REP']), ctrl.createReviewRequest);
+router.get('/requests', authorize(['INSTRUCTOR', 'INST_REP', 'ADMIN']), ctrl.getAllReviewRequests);
+router.get('/instructor', authorize(['INSTRUCTOR', 'ADMIN', 'INST_REP']), ctrl.getReviewRequestsByInstructor);
+router.post('/responses', authorize(['INSTRUCTOR', 'ADMIN', 'INST_REP']), ctrl.createReviewResponse);
+router.get('/status', authorize(['STUDENT', 'INSTRUCTOR', 'ADMIN', 'INST_REP']), ctrl.getReviewStatusForStudent);
+router.get('/requests/student/:studentId', authorize(['STUDENT', 'ADMIN', 'INSTRUCTOR', 'INST_REP']), ctrl.getReviewRequestsForStudent);
 
 module.exports = router;
