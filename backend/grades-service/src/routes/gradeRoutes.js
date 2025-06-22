4619cda33a2904a594419e9a90a7017196758260
Giannisgou <gousios.giannes@gmail.com>
2025-06-23 00:29:38 +0300

    testing done and some routes authorized

--- backend/grades-service/src/routes/gradeRoutes.js
+++ backend/grades-service/src/routes/gradeRoutes.js
@@ -11,8 +11,8 @@ router.get('/instructor/:id/courses', authorize(['INSTRUCTOR', 'ADMIN']), contro
 router.post('/:type', authorize(['INSTRUCTOR']), upload.single('file'), controller.handleUpload); // upload grades
 
 // 🔓 Public
-router.get('/distribution/:courseId/:type', controller.getTotalDistribution); // overall grade distribution
-router.get('/distribution/:courseId/:type/q/:question', controller.getQuestionDistribution); // per-question distribution
-router.get('/questions/:courseId/:type', controller.getQuestionKeys); // get question keys
+router.get('/distribution/:courseId/:type', authorize(['INSTRUCTOR', 'ADMIN']), controller.getTotalDistribution); // overall grade distribution
+router.get('/distribution/:courseId/:type/q/:question', authorize(['INSTRUCTOR', 'ADMIN']), controller.getQuestionDistribution); // per-question distribution
+router.get('/questions/:courseId/:type', authorize(['INSTRUCTOR', 'ADMIN']), controller.getQuestionKeys); // get question keys
 
 module.exports = router;
